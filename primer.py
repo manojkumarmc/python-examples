b9a84645d867d6f016e2a973c6c751afaec21aa9
Manoj Kumar MC <manoj.kumar.myaliputhenpurayi@oracle.com>
2015-07-16 09:00:39 +0530

    super efficient prime calculator

---
+++ primer.py
@@ -0,0 +1,16 @@
+
+from itertools import islice
+
+def is_prime(a):
+    return all(a % i for i in xrange(2, a))
+
+def prime_summer(n):
+    sum = 0
+    for x in xrange(1,n):
+        if is_prime(x):
+            sum += x
+    return sum
+
+print prime_summer(100)
+
+    
