ed64209156b36a327f9e6fe746312bb60e6d90a4
Manoj Kumar MC <mcbobs@Manojs-MacBook-Pro.local>
2015-07-18 12:22:45 +0530

    directory traversal

---
+++ dir_traversal.py
@@ -0,0 +1,14 @@
+
+import os
+
+rootdir = '/Users/mcbobs/projects/python/'
+
+def travel(mode=True):
+    for dir_name, sub_dir, file_list in os.walk(rootdir, topdown=mode):
+        print "Directory name is %s " % dir_name
+        for fl in file_list:
+            print "\t%s" % fl
+
+travel()
+travel(FOBalse)
+
