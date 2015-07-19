e676bacdfc560c12b9742d09909565dbcb1aa365
Manoj Kumar MC <mcbobs@Manojs-MacBook-Pro.local>
2015-07-20 00:37:49 +0530

    flattening and iter protocol

---
+++ flatter.py
@@ -0,0 +1,15 @@
+
+from collections import Iterable
+
+def flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from flatten(x)
+        else:
+            yield x
+
+alist = [1,[2,3,[4,5,[6,7]]]]
+
+for x in flatten(alist):
+    print(x)
+
