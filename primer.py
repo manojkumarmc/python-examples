e20ca2cb798411b7435a775773c60cb634ede687
Manoj Kumar MC <manoj.kumar.myaliputhenpurayi@oracle.com>
2015-12-03 13:23:11 +0530

    add display

--- primer.py
+++ primer.py
@@ -11,6 +11,13 @@ def prime_summer(n):
             sum += x
     return sum
 
-print prime_summer(100)
+def prime_lister(n):
+    for x in xrange(n):
+        if is_prime(x):
+            print x
+
+#print prime_summer(100)
+
+prime_lister(100)
 
     
