e2fd13c8bfed7b87338d2129b01752ac6d59e5b9
Manoj Kumar MC <manoj.kumar.myaliputhenpurayi@oracle.com>
2016-01-07 16:58:06 +0530

    Worker thread modified to manage multiple elevators

--- elevator/server.py
+++ elevator/server.py
@@ -4,7 +4,7 @@ from threading import Thread
 import time
 
 NUM_OF_FLOORS = 4
-NUM_OF_ELEVS  = 1
+NUM_OF_ELEVS  = 2
 
 def worker():
     current_floor = 0
@@ -12,7 +12,7 @@ def worker():
     while True:
         if not in_process:
             dest = q.get()
-        print "\n The floor being processed is " + str(dest)
+        print '\n The floor being processed is ' + str(dest)
 
         if int(dest) > current_floor:
             while current_floor < int(dest):
@@ -31,16 +31,53 @@ def worker():
         q.task_done()
 
 
+def multi_worker(elev_name=''):
+    current_floor = 0
+    in_process = False    
+    while True:
+        if not in_process:
+            dest = q.get()
+        print '\n'+ elev_name +' | Destination : ' + str(dest)
+
+        if int(dest) > current_floor:
+            while current_floor < int(dest):
+                in_process = True
+                time.sleep(1)
+                current_floor += 1
+		print '\n'+ elev_name +' | At level: ' + str(current_floor)
+		if current_floor in lst:
+		    print '\n'+ elev_name +' | Opening door at :' + str(current_floor)
+                    time.sleep(1)
+		    print '\n'+ elev_name +' | Closing door at :' + str(current_floor)
+		    filter(lambda a: a != current_floor, lst)	
+                
+        elif int(dest) < current_floor:
+            while current_floor > int(dest):
+                in_process = True
+                time.sleep(1)
+                current_floor -= 1
+                print '\n'+ elev_name +' | At level: ' + str(current_floor)
+		if current_floor in lst:
+		    print '\n'+ elev_name +' | Opening door at :' + str(current_floor)
+                    time.sleep(1)
+		    print '\n'+ elev_name +' | Closing door at :' + str(current_floor)
+		    filter(lambda a: a != current_floor, lst)	
+
+        in_process = False
+        q.task_done()    
+
+
 q = Queue()
-
-t = Thread(target=worker)
-t.daemon = True
-t.start()
-
+lst = []
+for elev in xrange(NUM_OF_ELEVS):
+    t = Thread(target=multi_worker, args=('Elev-' + str(elev + 1),))
+    t.daemon = True
+    t.start()
 
 while True:
-    level = raw_input("\nPlease enter your level: ")
+    level = raw_input('\nPlease enter your level: ')
     q.put(level)
+    lst.append(int(level))
 
 q.join()
 
