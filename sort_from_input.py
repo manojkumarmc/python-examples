8f0447703204e2ac8eceb3e51af4b2afe86e9f19
Manoj Kumar MC <manoj.chandrankutty@philips.com>
2017-01-19 16:59:00 +0530

    cleanup methods

--- sort_from_input.py
+++ sort_from_input.py
@@ -1,4 +1,5 @@
 from operator import itemgetter
+import re
 
 to_sort_list = [
     (10, 2, 9),
@@ -15,18 +16,25 @@ def sort_the_list(sort_key=0):
 def sl(sort_key=0):
     return sorted(to_sort_list, key=itemgetter(sort_key))
 
-# while True:
-#     print "1. Continue"
-#     print "2. Exit"
-#     print "_______________"
-#     s = raw_input('Please enter your value: ')
-#     if str(s).strip() != '2':
-#         print 'You entered %s ' % str(s)
-#     else:
-#         print "Exiting"
-#         break
+def sanitize_number(a_str):
+    return re.findall(r'\d+',a_str)
+
+while True:
+    print "1. Add numbers differenciated with space"
+    print "2. Sort the table"
+    print "3. Exit"
+    print "_______________"
+    s = raw_input('Please enter your value: ')
+    if str(s).strip() != '3':
+        print ""
+        s1 = raw_input("Enter values: ")
+        print 'You entered %s ' % str(s1)
+    else:
+        print "Exiting"
+        break
 
 # print(sort_the_list(1))
 # print(sl(2))
 
+# print(tuple(sanitize_number('is 100 greater than 9')))
 
