6122700268c46b3fe4c01282fc2ba2cdfc2bafc8
Reghunath Prakash <reghu@localhost.localdomain>
2017-01-21 00:32:19 +0530

    almost done

--- sort_from_input.py
+++ sort_from_input.py
@@ -1,13 +1,7 @@
 from operator import itemgetter
 import re
 
-to_sort_list = [
-    (10, 2, 9),
-    (8, 8, 8),
-    (12, 5, 4),
-    (1, 1, 1)
-]
-
+to_sort_list = []
 sort_key = 0
 
 def sort_the_list(sort_key=0):
@@ -17,7 +11,7 @@ def sl(sort_key=0):
     return sorted(to_sort_list, key=itemgetter(sort_key))
 
 def sanitize_number(a_str):
-    return re.findall(r'\d+',a_str)
+    return tuple(re.findall(r'\d+',a_str))
 
 while True:
     print "1. Add numbers differenciated with space"
@@ -25,16 +19,16 @@ while True:
     print "3. Exit"
     print "_______________"
     s = raw_input('Please enter your value: ')
-    if str(s).strip() != '3':
+    key_entered = str(s).strip()
+    if key_entered == '1':
         print ""
         s1 = raw_input("Enter values: ")
         print 'You entered %s ' % str(s1)
+        to_sort_list.append(sanitize_number(s1))
+    elif key_entered == '2':
+        print sl()
     else:
         print "Exiting"
         break
 
-# print(sort_the_list(1))
-# print(sl(2))
-
-# print(tuple(sanitize_number('is 100 greater than 9')))
 
