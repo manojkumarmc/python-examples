a3343f2d5ba6afe113a3186eb6aa19987af440ad
manojkumarmc <manojkumarmc@hotmail.com>
2017-01-24 10:31:40 +0530

    fraction functionality

--- iv-league/whole_fraction.py
+++ iv-league/whole_fraction.py
@@ -2,25 +2,28 @@ class Fraction(object):
 
     """Docstring for Fraction. """
 
-    def __init__(self,nom,denom):
-        """This class will take nominator and denominator
-
-        :nom: TODO
-        :denom: TODO
-
+    def __init__(self,n,d):
+        """This class will take numerator and denominator
         """
-        self._nom = nom
-        self._denom = denom
+        self._n = n
+        self._d = d
 
-    def common_divisor(self):
-        for n in range(2,10):
-            if self._nom / n >= 1:
-                if self._denom / n >= 1:
-                    print(n)
-                    break
+    @classmethod
+    def factors(self, n):
+        return sorted(set([int(n/x) for x in range(2,n) if int(n/x) > 1 and n%x == 0]))
 
+    @classmethod
+    def lcf(self, n):
+        return min(self.factors(n))
+
+    @classmethod
+    def common_divisor(self, n1 ,n2):
+        return set(self.factors(n1)) & set(self.factors(n2))
 
 
 
 f = Fraction(2,3)
-f.common_divisor()
+print (f.factors(100))
+print (f.lcf(100))
+print (f.common_divisor(10,100))
+
