40d2e3bb13531f8c3020e0266ef076e44bd146bb
Reghunath Prakash <reghu@localhost.localdomain>
2017-01-29 02:11:57 +0530

    some utilities added

--- iv-league/whole_fraction.py
+++ iv-league/whole_fraction.py
@@ -1,3 +1,5 @@
+from itertools import islice
+
 class Fraction(object):
 
     """Docstring for Fraction. """
@@ -13,8 +15,25 @@ class Fraction(object):
         return sorted(set([int(n/x) for x in range(2,n) if int(n/x) > 1 and n%x == 0]))
 
     @classmethod
-    def lcf(self, n):
+    def lcm(self, n):
         return min(self.factors(n))
+
+    @classmethod
+    def ucf(self, n1, n2):
+        pass
+
+    @classmethod
+    def is_prime(self, n1):
+        return all([n1 % x for x in range(2,n1)])
+
+    @classmethod
+    def get_primes(self):
+        ctr = 1
+        while True:
+            if self.is_prime(ctr):
+                yield ctr
+            ctr += 1
+
 
     @classmethod
     def common_divisor(self, n1 ,n2):
@@ -23,7 +42,12 @@ class Fraction(object):
 
 
 f = Fraction(2,3)
-print (f.factors(100))
-print (f.lcf(100))
-print (f.common_divisor(10,100))
+# print (f.factors(100))
+# print (f.lcm(100))
+# print (f.common_divisor(10,100))
 
+# for x in range(50):
+#     print(f.is_prime(x))
+
+for x in islice(f.get_primes(),1,30):
+    print(x)
