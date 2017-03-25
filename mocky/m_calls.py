689d7379aa9327757760f2633137015d512d2d35
Reghunath Prakash <reghu@localhost.localdomain>
2017-03-25 20:28:01 +0530

    test cases

--- mocky/m_calls.py
+++ mocky/m_calls.py
@@ -1,3 +1,32 @@
+def m_printer(a, b):
+    return '%s plus %s' % (a,b)
+
+
 def add(a, b):
-    print "HHHHHHHHHH"
     return a + b
+
+def multi(a, b):
+    return a * b
+
+def num_mgr(a, b):
+    return {
+        'sum': add(a, b),
+        'product': multi(a, b)
+    }
+
+
+class Employee():
+
+    def __init__(self, name, age, sex):
+        self._name = name
+        self._age  = age
+        self.sex   = sex
+
+    def get_name(self):
+        return self._name
+
+    def get_age(self):
+        return self._age
+
+    def get_sex(self):
+        return self._sex
--- mocky/test_mc.py
+++ mocky/test_mc.py
@@ -2,14 +2,28 @@
 from mock import Mock, patch
 import unittest
 
-from m_calls import add
+from m_calls import add, num_mgr
 
 class MyTestCase(unittest.TestCase):
 
-    @patch('m_calls.add', return_value = 100)
-    def test_my_add(self, mock_add):
+    def test_my_add(self):
         self.assertEqual(add(1, 2), 3)
+
+    @patch('m_calls.add', return_value = 100)
+    def test_my_add_mck(self, mock_add):
         self.assertEqual(mock_add(1, 2), 100)
+
+    @patch('m_calls.add')
+    @patch('m_calls.multi')
+    def test_num_mgr(self,  mock_multi, mock_add):
+        mock_add.return_value = 20
+        mock_multi.return_value = 100
+        self.assertEquals(num_mgr(1,1),
+                          {'sum':20,
+                           'product': 100}
+                          )
+
+
 
 if __name__ == '__main__':
     unittest.main()
