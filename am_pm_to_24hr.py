f32b978de676c54774ac36f4bf5502e9d272e17b
manojkumarmc <manojkumarmc@hotmail.com>
2018-04-21 15:18:30 +0530

    starting with hypothesis test cases

--- am_pm_to_24hr.py
+++ am_pm_to_24hr.py
@@ -1,8 +1,11 @@
-import sys
 
-time = raw_input().strip()
+def time_converter(time_str):
+    time_str = time_str.rjust(4,'0')
+    if time_str[-2:] == 'PM':
+        return str(int(time_str[0:2]) + 12) + time_str[2:len(time_str) - 2]
+    else:
+        return time_str.replace('PM', '').replace('AM', '')
 
-if time[-2:] == 'PM':
-    print str(int(time[0:2]) + 12) + time[2:len(time) - 2]
-else:
-    print time.replace('PM').replace('AM')
+
+# if __name__ == "__main__":
+#     print time_converter('1AM')
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -5,6 +5,7 @@ from hypothesis import strategies as st
 sys.path.append('/Users/mcbobs/projects/python/python-examples')
 
 from airport_list import list_airports
+from am_pm_to_24hr import time_converter
 
 class TestRunner(unittest.TestCase):
 
@@ -13,6 +14,12 @@ class TestRunner(unittest.TestCase):
     def test_airport_lister(self, s1, s2):
         ap = 'This flight goes from {} to {}'.format(s1, s2)
         self.assertEquals(len(list_airports(ap)), 2)
+
+    @given(p1=st.integers(min_value=1, max_value=12),
+           p2=st.sampled_from(['AM', 'PM']))
+    def test_time_converter(self, p1, p2):
+        time_str = str(p1) + p2
+        self.assertLessEqual(int(time_converter(time_str)), 24)
 
 
 
